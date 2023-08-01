01408018dcab724dab6875b9b8afac1005d316e9
Admin <Admin@DESKTOP-QV9DHBL>
2023-08-01 16:47:40 +0900

    .

---
+++ WinFormsApp018/WinFormsApp018/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp018
+{
+    public partial class Form1 : Form
+    {
+        private int cnt;
+        private Thread t1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t1 = new Thread(() =>
+            {
+                while (true) 
+                {
+                    try
+                    {
+                        Invoke((Action)(() => label1.Text = cnt++.ToString()));
+
+                        Thread.Sleep(1000);
+                    }
+                    catch(Exception ex) {
+                        Debug.WriteLine(ex.Message);
+                        break;
+                    }
+                }
+            });
+            t1.IsBackground = true;
+            t1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t1.Interrupt();
+        }
+    }
+}
