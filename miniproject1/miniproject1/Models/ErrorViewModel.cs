3f4874210db925a3568968e94dea80f27743f29a
Admin <Admin@DESKTOP-QV9DHBL>
2023-09-11 09:39:20 +0900

    .

--- miniproject1/miniproject1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace miniproject1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
